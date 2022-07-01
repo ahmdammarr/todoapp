a7016cb876d5a31530f646588962400c85f7fb60
AhmedAmmarr <ahmedmahmoud9597@gmail.com>
2022-07-01 20:45:59 +0300

    adding uselocalization hook

---
+++ src/shared/hooks/useLocalization/useLocalization.ts
@@ -0,0 +1,35 @@
+import i18n from 'i18n-js';
+import {ELanguages, EStorageKeys} from 'shared/enums';
+import {I18nManager} from 'react-native';
+import {setItem} from 'utils/localStorage';
+import RNRestart from 'react-native-restart';
+
+export const useLocalization = () => {
+    
+  const changeLanguage = async (language: ELanguages) => {
+    if (i18n.currentLocale() === language) {
+      return;
+    } else {
+      await setItem(EStorageKeys.language, language);
+      i18n.locale = language;
+      I18nManager.forceRTL(language === ELanguages.ar);
+      RNRestart.Restart();
+    }
+  };
+
+  function translate(name: string, params = {}) {
+    return i18n.t(name, params);
+  }
+
+  const getCurrentLanguage = () => i18n.currentLocale();
+  const isArabic = i18n.currentLocale() === ELanguages.ar;
+  const isRTL = I18nManager.isRTL;
+
+  return {
+    changeLanguage,
+    isArabic,
+    isRTL,
+    translate,
+    getCurrentLanguage,
+  };
+};
