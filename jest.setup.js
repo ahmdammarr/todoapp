3654c4fa82392fcb964abfbb81ed445f387deffa
AhmedAmmarr <ahmedmahmoud9597@gmail.com>
2022-07-01 16:32:43 +0300

    initialization with testing config

---
+++ jest.setup.js
@@ -0,0 +1,43 @@
+import '@testing-library/jest-native/extend-expect'
+import {jest} from '@jest/globals'
+
+import Promise from 'promise-polyfill'
+global.Promise = Promise
+
+jest.mock('react-native-gesture-handler', () => {
+  const View = require('react-native/Libraries/Components/View/View')
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    /* Buttons */
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    /* Other */
+    FlatList: View,
+    gestureHandlerRootHOC: () => null,
+    Directions: {},
+  }
+})
+
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
+
