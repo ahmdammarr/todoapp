0b178286e7f39d7c4bbc1ec1b328018f2a6685e1
AhmedAmmarr <ahmedmahmoud9597@gmail.com>
2022-07-01 16:53:18 +0300

    config/absolute-paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ["module-resolver", {
+      root: ['src'],
+      extensions: ['.js', '.jsx', '.es', '.es6', '.mjs', '.ts', '.tsx']
+    }]
+  ]
 };
