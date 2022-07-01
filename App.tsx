65681b26f10ffcb4fabd0991282c8400ecd396d8
AhmedAmmarr <ahmedmahmoud9597@gmail.com>
2022-07-01 21:51:23 +0300

    adding cached resources hook

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import {Main} from 'Main';
-
+import { useCachedResources } from 'shared/hooks/useCashedResources';
+import { ActivityIndicator } from 'react-native';
+import 'i18n'
 const App = () => {
+  const {isLoading} = useCachedResources()
+  if(isLoading) return <ActivityIndicator/>
   return <Main />;
 };
 
---
+++ src/shared/hooks/useCashedResources/__tests__/useCachedResources.test.tsx
@@ -0,0 +1,21 @@
+import 'react-native';
+import React from 'react';
+import {useCachedResources} from '../useCachedResources';
+import {render} from '@testing-library/react-native';
+import {Main} from 'Main';
+
+describe.only('CachedResources hook', () => {
+  it('renders main correclty', () => {
+    let results;
+    function HookWrapper() {
+      results = useCachedResources();
+      return null;
+    }
+    render(<HookWrapper />);
+
+    if (!results.isLoading) {
+      expect(render(<Main />)).toBeTruthy();
+      //   console.log('r',r.rerender)
+    }
+  });
+});
