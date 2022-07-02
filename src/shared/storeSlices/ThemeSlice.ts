0fcc44010ed8488d6f8c7d5ee4bd12b83e090f71
AhmedAmmarr <ahmedmahmoud9597@gmail.com>
2022-07-02 11:35:51 +0300

    adding theme store slice

---
+++ src/shared/storeSlices/ThemeSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit"
+import {useColorScheme } from 'react-native';
+import { EStores } from "shared/models/enums/store.enums";
+import { TRootState } from "shared/models/types/store.types";
+import { useAppSelector } from "shared/hooks/useAppSelector";
+
+const defaultMood = useColorScheme()
+export const themeSlice = createSlice({
+  name: EStores.theme,
+  initialState: defaultMood ,
+  reducers: {
+    setTheme: (state, action)=> state = action.payload
+  }
+})
+
+
+
+export const { setTheme } = themeSlice.actions
+
+const themeReducer = themeSlice.reducer;
+const selectTheme = (state:Pick<TRootState,EStores.theme>) => state.theme
+const useTheme = () => useAppSelector(selectTheme);
+
+
+export {
+    themeReducer,
+    useTheme
+}
